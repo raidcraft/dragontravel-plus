ab5ae3ce014fea47e1d09f30d4c9bc3391cf0371
Philip Urban <urban.philip@googlemail.com>
2012-11-22 06:03:21 +0100

    init commit

---
+++ src/main/java/de/raidcraft/dragontravelplus/DragonTravelPlusModule.java
@@ -0,0 +1,22 @@
+package de.raidcraft.dragontravelplus;
+
+import com.zachsthings.libcomponents.ComponentInformation;
+import com.zachsthings.libcomponents.Depend;
+import com.zachsthings.libcomponents.bukkit.BukkitComponent;
+
+/**
+ * Author: Philip
+ * Date: 22.11.12 - 06:01
+ * Description:
+ */
+@ComponentInformation(
+        friendlyName = "Dragon Travel Plus Module",
+        desc = "Sends the dragons into the air."
+)
+public class DragonTravelPlusModule extends BukkitComponent {
+
+    @Override
+    public void enable() {
+
+    }
+}
