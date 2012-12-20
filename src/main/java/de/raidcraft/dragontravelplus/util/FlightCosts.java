a8a811239353af29ae9e54e232f7465d4b8e1068
Philip Urban <urban.philip@googlemail.com>
2012-12-20 22:26:34 +0100

    fixed flight cost calculation

--- src/main/java/de/raidcraft/dragontravelplus/util/FlightCosts.java
+++ src/main/java/de/raidcraft/dragontravelplus/util/FlightCosts.java
@@ -11,7 +11,17 @@ import de.raidcraft.dragontravelplus.station.DragonStation;
 public class FlightCosts {
 
     public static double getPrice(DragonStation start, DragonStation destination) {
-        return Math.round(start.getCostLevel() * start.getLocation().distance(destination.getLocation()) * DragonTravelPlusModule.inst.config.pricePerBlock * 100.) / 100.;
+        if(destination.isEmergencyTarget() || (start.getCostLevel() == 0 && destination.getCostLevel() == 0)) {
+            return 0;
+        }
+        int costLevel = 1;
+        if(destination.getCostLevel() == 0) {
+            costLevel = destination.getCostLevel();
+        }
+        else {
+            costLevel = start.getCostLevel();
+        }
+        return Math.round(costLevel * start.getLocation().distance(destination.getLocation()) * DragonTravelPlusModule.inst.config.pricePerBlock * 100.) / 100.;
     }
 
 }
