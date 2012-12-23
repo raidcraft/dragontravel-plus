40774eac3391e0a31c4ffee06b1a7d2ddcb0d2aa
Philip Urban <urban.philip@googlemail.com>
2012-12-23 18:02:44 +0100

    hopefully fixed stucking dragons

--- src/main/java/de/raidcraft/dragontravelplus/dragoncontrol/dragon/RCDragon.java
+++ src/main/java/de/raidcraft/dragontravelplus/dragoncontrol/dragon/RCDragon.java
@@ -11,25 +11,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 
-/**
- * Copyright (C) 2011-2012 Moser Luca/Philipp Wagner
- * moser.luca@gmail.com/mail@phiwa.eu
- * 
- * This file is part of DragonTravel.
- * 
- * DragonTravel is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- * 
- * DragonTravel is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * Foobar. If not, see <http://www.gnu.org/licenses/>.
- */
 public class RCDragon extends EntityEnderDragon {
 
 	// Travel
@@ -236,7 +217,9 @@ public class RCDragon extends EntityEnderDragon {
 
 		// If myZ = toZ, then we will load the next waypoint or
 		// finish the flight, in case it was the last waypoint to fly
-		if ((int) myZ == (int) toZ && (int) myY == (int) toY && (int) myX == (int) toX) {
+		if (((int) myZ >= (int) toZ-2 &&  (int)myZ <= (int) toZ+2)
+                && ((int) myY >= (int) toY-2 &&  (int)myY <= (int) toY+2)
+                && ((int) myX >= (int) toX-2 &&  (int)myX <= (int) toX+2)) {
 			Waypoint wp = flight.getNextWaypoint();
 
 			// Removing the entity and dismouting the player
