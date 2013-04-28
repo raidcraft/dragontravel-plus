d16d907e13748fc9301ec3b501261d4e7d53dd39
Silthus <silthus@raid-craft.de>
2013-04-28 13:50:25 +0200

    trying to hide the dragon nameplate

--- src/main/java/de/raidcraft/dragontravelplus/dragoncontrol/FlyingPlayer.java
+++ src/main/java/de/raidcraft/dragontravelplus/dragoncontrol/FlyingPlayer.java
@@ -1,9 +1,17 @@
 package de.raidcraft.dragontravelplus.dragoncontrol;
 
+import com.comphenix.protocol.Packets;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ConnectionSide;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import de.raidcraft.RaidCraft;
+import de.raidcraft.api.packets.Packet28EntityMetadata;
 import de.raidcraft.dragontravelplus.DragonTravelPlusPlugin;
 import de.raidcraft.dragontravelplus.dragoncontrol.dragon.RCDragon;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 /**
@@ -13,9 +21,11 @@ import org.bukkit.entity.Player;
  */
 public class FlyingPlayer {
 
-    private Player player;
+    private static final int ENTITY_NAME_INVISIBLE = 0x0;
+
+    private final Player player;
+    private final Location start;
     private RCDragon dragon = null;
-    private Location start;
     private Location destination;
     private boolean inAir = false;
     private int waitingTaskID = 0;
@@ -24,8 +34,7 @@ public class FlyingPlayer {
 
     public FlyingPlayer(Player player, Location start, Location destination, double price) {
 
-        this.player = player;
-        this.start = start;
+        this(player, start);
         this.destination = destination;
         this.price = price;
     }
@@ -34,6 +43,28 @@ public class FlyingPlayer {
 
         this.player = player;
         this.start = start;
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(
+                RaidCraft.getComponent(DragonTravelPlusPlugin.class),
+                ConnectionSide.SERVER_SIDE,
+                Packets.Server.ENTITY_METADATA
+        ) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+
+                Packet28EntityMetadata packet = new Packet28EntityMetadata(event.getPacket());
+                Entity entity = packet.getEntity(event);
+
+                if (getDragon() != null && getDragon().getBukkitEntity().equals(entity)) {
+
+                    // Clone and update it
+                    packet = new Packet28EntityMetadata(packet.getHandle().deepClone());
+                    WrappedDataWatcher watcher = new WrappedDataWatcher(packet.getEntityMetadata());
+                    watcher.setObject(6, (byte) ENTITY_NAME_INVISIBLE);
+
+                    event.setPacket(packet.getHandle());
+                }
+            }
+        });
     }
 
     public boolean hasIncorrectState() {
