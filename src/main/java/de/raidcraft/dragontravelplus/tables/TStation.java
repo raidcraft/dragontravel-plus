97479f3c2f40a7fddd2afdda119b354923b12eff
Silthus <silthus@raid-craft.de>
2014-03-01 22:23:49 +0100

    maybe this will help

--- src/main/java/de/raidcraft/dragontravelplus/tables/TStation.java
+++ src/main/java/de/raidcraft/dragontravelplus/tables/TStation.java
@@ -6,7 +6,6 @@ import org.bukkit.Location;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.ArrayList;
@@ -31,11 +30,9 @@ public class TStation {
     private double costMultiplier = 1.0;
     private boolean mainStation = false;
     private boolean emergencyStation = false;
-    @OneToMany
-    @JoinColumn(name = "start_station_id")
+    @OneToMany(mappedBy = "start_station_id")
     private List<TPath> paths = new ArrayList<>();
     @OneToMany
-    @JoinColumn(name = "station_id")
     private List<TPlayerStation> playerStations = new ArrayList<>();
 
     public int getId() {
