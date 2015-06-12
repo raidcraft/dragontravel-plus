1887c8f17cb41a90636d3d3b8f3471112c37c527
Philip <urban.philip@gmail.com>
2015-06-12 23:38:08 +0200

    improved flight gui

--- plugin/src/main/java/de/raidcraft/dragontravelplus/flights/DragonStationFlight.java
+++ plugin/src/main/java/de/raidcraft/dragontravelplus/flights/DragonStationFlight.java
@@ -13,6 +13,9 @@ import de.raidcraft.dragontravelplus.station.DragonStation;
 import de.raidcraft.dragontravelplus.util.GUIUtil;
 import de.raidcraft.rctravel.api.station.Chargeable;
 import de.raidcraft.rctravel.api.station.Station;
+import de.raidcraft.skills.SkillsPlugin;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.hero.Option;
 import de.raidcraft.util.LocationUtil;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
@@ -74,7 +77,9 @@ public class DragonStationFlight extends RestrictedFlight {
             }
 
             // start thread to update GUI (distance view in title bar)
-            updateGUITaskID = Bukkit.getScheduler().runTaskTimer(RaidCraft.getComponent(DragonTravelPlusPlugin.class), new UpdateGuiTask(), 20, 30).getTaskId();
+            Hero hero = RaidCraft.getComponent(SkillsPlugin.class).getCharacterManager().getHero((Player)getPassenger().getEntity());
+            Option.ACTION_BAR.set(hero, false); // disable actionbar
+            updateGUITaskID = Bukkit.getScheduler().runTaskTimer(RaidCraft.getComponent(DragonTravelPlusPlugin.class), new UpdateGuiTask(), 20, 20).getTaskId();
         }
         super.onStartFlight();
     }
@@ -85,6 +90,8 @@ public class DragonStationFlight extends RestrictedFlight {
         // stop GUI update task
         if(updateGUITaskID != 0) {
             Bukkit.getScheduler().cancelTask(updateGUITaskID);
+            Hero hero = RaidCraft.getComponent(SkillsPlugin.class).getCharacterManager().getHero((Player)getPassenger().getEntity());
+            Option.ACTION_BAR.set(hero, true); // enable actionbar
         }
 
         // lets substract the flight cost
@@ -104,12 +111,51 @@ public class DragonStationFlight extends RestrictedFlight {
 
     private class UpdateGuiTask implements Runnable {
 
+        private int arrivalTime;
+        private int lastDistance;
+
         @Override
         public void run() {
+
+            int newDistance = (int)Math.round(getPassenger().getEntity().getLocation().distance(getEndLocation()));
+            int totalDistance = (int)Math.round(getEndLocation().distance(getStartLocation()));
+
+            /**
+             * Arrival Time Calculation
+             */
+            int blockPerSeconds = lastDistance - newDistance;
+            if(blockPerSeconds != 0) {
+                arrivalTime = totalDistance / blockPerSeconds;
+            }
+            String arrivalTimeString;
+
+            if(arrivalTime < 60) {
+                arrivalTimeString = ChatColor.GOLD.toString() + arrivalTime + "s";
+            } else {
+                arrivalTimeString = ChatColor.GOLD.toString() + (arrivalTime/60) + "min";
+            }
+
+            /**
+             * Distance Calculation
+             */
+            String distanceString;
+
+            if(newDistance > 1000) {
+                distanceString = ChatColor.GOLD.toString() + (double)Math.round(((double)newDistance/1000D)*100)/100D + "km";
+            } else {
+                distanceString = ChatColor.GOLD.toString() + newDistance + "m";
+            }
+
+
+                    lastDistance = newDistance;
+
             GUIUtil.setTitleBarText((Player)getPassenger().getEntity(),
                     ChatColor.DARK_GRAY + "*** " +
                             ChatColor.DARK_PURPLE + "Entfernung zum Ziel: " +
-                            ChatColor.GOLD + getStartLocation().distance(getEndLocation()) + "m" + ChatColor.DARK_GRAY + " ***");
+                            ChatColor.GOLD + newDistance + distanceString +
+                            ChatColor.DARK_GRAY + "| " +
+                            ChatColor.DARK_PURPLE + "Ankunft in " + arrivalTimeString +
+                            ChatColor.DARK_GRAY + "***");
         }
     }
 }
