3d14f774b1ca31958ff0866dd7d18853d4224562
Dustin <keiler20690@gmail.com>
2015-07-13 19:10:52 +0200

    fixed conversations dependency

--- plugin/src/main/java/de/raidcraft/dragontravelplus/npc/DragonGuardManager.java
+++ plugin/src/main/java/de/raidcraft/dragontravelplus/npc/DragonGuardManager.java
@@ -5,7 +5,6 @@ import de.raidcraft.api.npc.NPC_Manager;
 import de.raidcraft.dragontravelplus.DragonTravelPlusPlugin;
 import de.raidcraft.dragontravelplus.StationManager;
 import de.raidcraft.dragontravelplus.station.DragonStation;
-import de.raidcraft.rcconversations.npc.NPC_Conservations_Manager;
 import de.raidcraft.rctravel.api.station.Station;
 import de.raidcraft.rctravel.npc.StationTrait;
 import net.citizensnpcs.api.npc.NPC;
@@ -23,9 +22,9 @@ public class DragonGuardManager {
         Location improvedLocation = station.getLocation().clone();
         improvedLocation.setY(improvedLocation.getY() + 1.5);
         DragonTravelPlusPlugin plugin = RaidCraft.getComponent(DragonTravelPlusPlugin.class);
-        NPC npc = NPC_Conservations_Manager.getInstance().spawnNonPersistNpcConservations(improvedLocation, "Drachenmeister", plugin.getName(), plugin.getConfig().conversationName);
-        npc.addTrait(StationTrait.class);
-        npc.getTrait(StationTrait.class).setStationName(station.getName());
+        //NPC npc = NPC_Conservations_Manager.getInstance().spawnNonPersistNpcConservations(improvedLocation, "Drachenmeister", plugin.getName(), plugin.getConfig().conversationName);
+        //npc.addTrait(StationTrait.class);
+        //npc.getTrait(StationTrait.class).setStationName(station.getName());
     }
 
     public static void spawnAllDragonGuardNPCs(StationManager stationManager) {
