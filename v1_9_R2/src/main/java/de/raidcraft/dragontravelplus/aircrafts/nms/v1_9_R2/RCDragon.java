a5136186d5758b209a2b24cbea7b66a3e0af8c5c
Philip <urban.philip@gmail.com>
2016-06-05 15:40:14 +0200

    Fixed names to match used NMS version

--- v1_9_R2/src/main/java/de/raidcraft/dragontravelplus/aircrafts/nms/v1_9_R2/RCDragon.java
+++ v1_9_R2/src/main/java/de/raidcraft/dragontravelplus/aircrafts/nms/v1_9_R2/RCDragon.java
@@ -1,4 +1,4 @@
-package de.raidcraft.dragontravelplus.aircrafts.nms.v1_9_R1;
+package de.raidcraft.dragontravelplus.aircrafts.nms.v1_9_R2;
 
 import de.raidcraft.api.flight.aircraft.Aircraft;
 import de.raidcraft.api.flight.flight.Flight;
@@ -6,10 +6,10 @@ import de.raidcraft.api.flight.flight.Waypoint;
 import de.raidcraft.util.LocationUtil;
 import lombok.Getter;
 import lombok.Setter;
-import net.minecraft.server.v1_9_R1.EntityEnderDragon;
-import net.minecraft.server.v1_9_R1.World;
+import net.minecraft.server.v1_9_R2.EntityEnderDragon;
+import net.minecraft.server.v1_9_R2.World;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_9_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.scheduler.BukkitTask;
